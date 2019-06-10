af8969f435bb91c6cd6df5e9b77a3b4ec5c61920
DeltaHao <32171957+DeltaHao@users.noreply.github.com>
2019-06-10 23:21:40 +0800

    Revert "检查"
    This reverts commit 628d1191ec66aef1d8b6e6a90f4e4e6ed553ab46.

--- Lost-Found-Platform/losthub/losthub/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # 使用include调用app中的文件
-import main.urls  # 使用main中的urls.py处理链接跳转
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(main.urls))  # 将链接交由main中的urls.py处理
-]
